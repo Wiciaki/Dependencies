84ab394fc72f133a73a3ec86356759ad7cddaa11
Wiciaki <wicia_6@wp.pl>
2016-10-07 23:54:22 +0200

    This build does the job! Sadly, the icon is crap. :)

---
+++ SparkTech.Loader/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace SparkTech.Loader
+{
+    [SuppressMessage("ReSharper", "LocalizableElement")]
+    internal static class Program
+    {
+        private struct Handlable
+        {
+            public string Name;
+
+            public string CloudPath;
+
+            public string LocalPath;
+        }
+
+        private static readonly List<Handlable> Files;
+
+        private const string BaseWebPath = "https://raw.githubusercontent.com/Wiciaki/Dependencies/master/Download/";
+
+        static Program()
+        {
+            var librariesPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "EloBuddy", "Addons", "Libraries");
+
+            var libs = new List<string> { "SparkTech.SDK", "NLog", "MoreLinq" };
+            Files = new List<Handlable>(libs.Count);
+
+            foreach (var name in libs)
+            {
+                var dll = name + ".dll";
+
+                Files.Add(new Handlable
+                {
+                    Name = name, CloudPath = BaseWebPath + dll, LocalPath = Path.Combine(librariesPath, dll)
+                });
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            if (args == null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+            
+            Console.Write("Welcome! EloBuddy is starting soon...\n\n");
+
+            var dir = Directory.GetCurrentDirectory();
+
+            var updater = Path.Combine(dir, "Updater.exe");
+            {
+                var info = new FileInfo(updater);
+
+                if (info.Exists)
+                {
+                    info.Delete();
+                }
+            }
+            
+            string loader;
+
+            using (var client = new WebClient())
+            {
+                var assemblyName = typeof(Program).Assembly.GetName();
+                var web = client.DownloadString(BaseWebPath + assemblyName.Name + ".txt");
+
+                if (assemblyName.Version < new Version(web))
+                {
+                    File.WriteAllBytes(updater, Properties.Resources.Updater);
+                    ExitTo(updater);
+                }
+
+                // ReSharper disable once PossibleNullReferenceException
+                if (!new DirectoryInfo(dir).Name.Equals("elobuddy", StringComparison.OrdinalIgnoreCase) || (loader = Directory.GetFiles(dir).Select(Path.GetFileName).SingleOrDefault(x => x.Equals("elobuddy.loader.exe", StringComparison.OrdinalIgnoreCase))) == null)
+                {
+                    Console.WriteLine("This file must be placed in EloBuddy installation folder!\nPlease move this file and try again.");
+                    Console.Read();
+                    return;
+                }
+
+                foreach (var file in Files)
+                {
+                    Console.Write($@"Downloading {file.Name}...");
+                    client.DownloadFile(file.CloudPath, file.LocalPath);
+                    Console.WriteLine(@" done!");
+                }
+            }
+
+            ExitTo(loader);
+        }
+
+        private static void ExitTo(string fileName)
+        {
+            Process.Start(fileName);
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ SparkTech.Loader/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SparkTech.Loader")]
+[assembly: AssemblyDescription("This is an unofficial EloBuddy launcher to allow SparkTech.SDK addons!")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SparkTech")]
+[assembly: AssemblyProduct("SparkTech.Loader")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d2be0da3-9047-44df-a838-c8420dd7475b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
