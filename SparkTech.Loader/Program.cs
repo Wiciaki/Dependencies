9063520be86a8af404e05be449cdb8f20fd61df8
Wiciaki <wicia_6@wp.pl>
2016-10-08 14:01:37 +0200

    Loader 1.0.0.0 -> 1.0.0.1

--- SparkTech.Loader/Program.cs
+++ SparkTech.Loader/Program.cs
@@ -1,55 +1,92 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Linq;
-using System.Net;
-
-namespace SparkTech.Loader
+﻿namespace SparkTech.Loader
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Threading;
+
+    using Microsoft.Win32;
+
+    /// <summary>
+    /// The <see cref="Program"/> class
+    /// </summary>
     [SuppressMessage("ReSharper", "LocalizableElement")]
     internal static class Program
     {
+        /// <summary>
+        /// Holds data for a specified resource
+        /// </summary>
         private struct Handlable
         {
+            /// <summary>
+            /// The resource name
+            /// </summary>
             public string Name;
 
+            /// <summary>
+            /// The download link for a resource
+            /// </summary>
             public string CloudPath;
 
+            /// <summary>
+            /// The destination of a resource
+            /// </summary>
             public string LocalPath;
         }
 
+        /// <summary>
+        /// The resource data
+        /// </summary>
         private static readonly List<Handlable> Files;
 
+        /// <summary>
+        /// The beginning for download links
+        /// </summary>
         private const string BaseWebPath = "https://raw.githubusercontent.com/Wiciaki/Dependencies/master/Download/";
 
+        /// <summary>
+        /// The minimum .NET Framework version
+        /// </summary>
+        private const int MinRecommendedDotNetVersion = 394802;
+
+        /// <summary>
+        /// Initializes static members of the <see cref="Program"/> class
+        /// </summary>
         static Program()
         {
             var librariesPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "EloBuddy", "Addons", "Libraries");
+            Directory.CreateDirectory(librariesPath);
 
-            var libs = new List<string> { "SparkTech.SDK", "NLog", "MoreLinq" };
-            Files = new List<Handlable>(libs.Count);
+            var libs = new List<string> { "SparkTech.SDK", "NLog", "MoreLinq" /*, "JetBrains.Annotations" */ };
 
-            foreach (var name in libs)
+            Files = libs.ConvertAll(name =>
             {
                 var dll = name + ".dll";
 
-                Files.Add(new Handlable
-                {
-                    Name = name, CloudPath = BaseWebPath + dll, LocalPath = Path.Combine(librariesPath, dll)
-                });
-            }
+                return new Handlable { Name = name, CloudPath = BaseWebPath + dll, LocalPath = Path.Combine(librariesPath, dll) };
+            });
         }
 
+        /// <summary>
+        /// The entry point for an application
+        /// </summary>
+        /// <param name="args">The empty, non-null string array</param>
         private static void Main(string[] args)
         {
             if (args == null)
             {
                 throw new ArgumentNullException(nameof(args));
             }
-            
-            Console.Write("Welcome! EloBuddy is starting soon...\n\n");
+
+            if (!FrameworkUpdated())
+            {
+                Console.Write("It's recommended to have .NET Framework 4.6.2 or newer installed which was not detected on your system. Continuing anyway...\n\n");
+                Thread.Sleep(3000);
+            }
 
             var dir = Directory.GetCurrentDirectory();
 
@@ -63,7 +100,7 @@ namespace SparkTech.Loader
                 }
             }
             
-            string loader;
+            string elobuddy;
 
             using (var client = new WebClient())
             {
@@ -77,12 +114,14 @@ namespace SparkTech.Loader
                 }
 
                 // ReSharper disable once PossibleNullReferenceException
-                if (!new DirectoryInfo(dir).Name.Equals("elobuddy", StringComparison.OrdinalIgnoreCase) || (loader = Directory.GetFiles(dir).Select(Path.GetFileName).SingleOrDefault(x => x.Equals("elobuddy.loader.exe", StringComparison.OrdinalIgnoreCase))) == null)
+                if (!new DirectoryInfo(dir).Name.Equals("elobuddy", StringComparison.OrdinalIgnoreCase) || (elobuddy = Directory.GetFiles(dir).Select(Path.GetFileName).SingleOrDefault(x => x.Equals("elobuddy.loader.exe", StringComparison.OrdinalIgnoreCase))) == null)
                 {
                     Console.WriteLine("This file must be placed in EloBuddy installation folder!\nPlease move this file and try again.");
                     Console.Read();
                     return;
                 }
+
+                Console.Write("Welcome! EloBuddy is starting soon...\n\n");
 
                 foreach (var file in Files)
                 {
@@ -92,13 +131,27 @@ namespace SparkTech.Loader
                 }
             }
 
-            ExitTo(loader);
+            ExitTo(elobuddy);
         }
 
+        /// <summary>
+        /// Exits the current process and starts another one
+        /// </summary>
+        /// <param name="fileName"></param>
         private static void ExitTo(string fileName)
         {
             Process.Start(fileName);
             Environment.Exit(0);
+        }
+
+        /// <summary>
+        /// Determines whether at least the recommended version of 
+        /// </summary>
+        /// <returns></returns>
+        private static bool FrameworkUpdated()
+        {
+            var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full");
+            return key != null && Convert.ToUInt32(key.GetValue("Release", 0)) > MinRecommendedDotNetVersion;
         }
     }
 }
